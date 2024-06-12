885729f2143a4ae2411fd4ab8fd65c47f4991bec
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-06-11 23:10:58 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Providers } from './src/providers'
+import { Home } from './src/screens/home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Providers>
+      <Home />
+    </Providers>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
